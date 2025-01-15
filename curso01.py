6545fe74e6e5b4ff41e44c834a16abe6ba8b4242
Oscar Cuadrado Mendez <asturmaps@gmail.com>
2025-01-15 09:47:21 +0000

    modificacion de todo el percal

--- curso01.py
+++ curso01.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib as mp
+import pandas as pd
+
 print("Hola Mundo")
 print("empezando a trabajar en la nube")
 # Esto es un comentario
+for i in range(10):
+    print("La la")
